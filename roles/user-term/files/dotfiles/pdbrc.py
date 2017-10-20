4d12f2b350cfb104fb4e0bd60bcbb375f7286949
Leandro Lisboa Penz <lpenz@arista.com>
2017-10-20 15:02:27 +0100

    user-term: add history to pdb

---
+++ roles/user-term/files/dotfiles/pdbrc.py
@@ -0,0 +1,16 @@
+
+def _pdbrc_init():
+    # Save history across sessions
+    import readline
+    histfile = ".pdb-pyhist"
+    try:
+        readline.read_history_file(histfile)
+    except IOError:
+        pass
+    import atexit
+    atexit.register(readline.write_history_file, histfile)
+    readline.set_history_length(500)
+
+
+_pdbrc_init()
+del _pdbrc_init
