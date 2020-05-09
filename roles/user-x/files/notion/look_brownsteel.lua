f2f01cda2af3587b37a1f74f58a8c5412f78df37
Leandro Lisboa Penz <lpenz@lpenz.org>
2020-05-09 15:44:22 +0100

    user-x/notion: use more specific font in notion
    Using terminus instead of helvetica.

--- roles/user-x/files/notion/look_brownsteel.lua
+++ roles/user-x/files/notion/look_brownsteel.lua
@@ -13,12 +13,12 @@ de.defstyle("*", {
     highlight_pixels = 1,
     shadow_pixels = 1,
     border_style = "elevated",
-    font = "-*-helvetica-medium-r-normal-*-14-*-*-*-*-*-*-*",
+    font = "-*-terminus-medium-*-*-*-14-*-*-*-*-*-*-*",
     text_align = "center",
 })
 
 de.defstyle("tab", {
-    font = "-*-helvetica-medium-r-normal-*-12-*-*-*-*-*-*-*",
+    font = "-*-terminus-medium-*-*-*-14-*-*-*-*-*-*-*",
     de.substyle("active-selected", {
         shadow_colour = "#304050",
         highlight_colour = "#708090",
