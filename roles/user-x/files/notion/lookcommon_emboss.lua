68cf0716ebee92937b45cf8afbd7786ca6b58e3e
Leandro Lisboa Penz <lpenz@lpenz.org>
2021-01-10 17:00:06 +0000

    notion: make config compatible with new notion release

--- roles/user-x/files/notion/lookcommon_emboss.lua
+++ roles/user-x/files/notion/lookcommon_emboss.lua
@@ -2,9 +2,7 @@
 
 de.defstyle("frame", {
     background_colour = "#000000",
-    -- The special "inherit" value causes setting 'background_colour'
-    -- above not to set padding_colour, but this colour being inherited.
-    padding_colour = "inherit",
+    padding_colour = "#000000",
     de.substyle("quasiactive", {
         -- Something detached from the frame is active
         padding_colour = "#901010",
@@ -25,14 +23,14 @@ de.defstyle("frame-tiled", {
     padding_pixels = 1,
 })
 
---de.defstyle("frame-tiled-alt", {
---    bar = "none",
---})
-
 de.defstyle("frame-floating", {
     --bar = "shaped",
     spacing = 0,
 })
+
+-- de.defstyle("frame-tiled-alt", {
+--     bar = "none",
+-- })
 
 de.defstyle("frame-transient", {
     --bar = "none",
@@ -46,29 +44,28 @@ de.defstyle("actnotify", {
     foreground_colour = "#ffffff",
 })
 
-de.defstyle("tab", {
+de.defstyle("tab-frame", {
     de.substyle("*-*-*-unselected-activity", {
         shadow_colour = "#600808",
         highlight_colour = "#c04040",
         background_colour = "#901010",
         foreground_colour = "#eeeeee",
     }),
-    
+
     de.substyle("*-*-*-selected-activity", {
         shadow_colour = "#600808",
         highlight_colour = "#c04040",
         background_colour = "#b03030",
         foreground_colour = "#ffffff",
     }),
-    
+
     de.substyle("*-*-*-tabnumber", {
         background_colour = "black",
         foreground_colour = "green",
     }),
 })
 
-
-de.defstyle("tab-frame", {
+de.defstyle("tab-frame-tiled", {
     spacing = 1,
 })
 
@@ -90,7 +87,6 @@ de.defstyle("tab-menuentry-big", {
     font = "-*-helvetica-medium-r-normal-*-17-*-*-*-*-*-*-*",
     padding_pixels = 7,
 })
-
 
 de.defstyle("stdisp", {
     shadow_pixels = 0,
